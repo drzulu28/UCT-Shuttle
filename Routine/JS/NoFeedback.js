60100a2b799e089d6c5fce5919884556cf82312a
drzulu28 <drzulu28@gmail.com>
2024-10-13 20:40:02 +0200

    Update NoFeedback.js
    Handle back button action

--- Routine/JS/NoFeedback.js
+++ Routine/JS/NoFeedback.js
@@ -1,3 +1,6 @@
 function handleNoClick() {
     window.location.href = 'feedback.html'; // Redirect to feedback page
 }
+function handleBackClick() {
+    window.location.href = 'schedule.html'; // Redirect to schedule page
+}
