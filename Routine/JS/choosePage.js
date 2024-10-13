412b181524d7c912860913f0926feb049fa03ae9
drzulu28 <drzulu28@gmail.com>
2024-10-13 18:28:15 +0200

    Add files via upload
    authorized user have a selection option

---
+++ Routine/JS/choosePage.js
@@ -0,0 +1,8 @@
+// Function to redirect based on user's choice
+function goToPage(page) {
+    if (page === 'schedule') {
+        window.location.href = "schedule.html"; // Update with actual URL for schedule page
+    } else if (page === 'feedback') {
+        window.location.href = "UserFeedbackHistory.html"; // Update with actual URL for feedback page
+    }
+}
