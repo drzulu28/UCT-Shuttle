e2004092cda6ce8ef7af9eaa5f14ac5634ba1cf6
drzulu28 <drzulu28@gmail.com>
2024-10-17 23:45:51 +0200

    Update choosePage.js

--- Routine/JS/choosePage.js
+++ Routine/JS/choosePage.js
@@ -1,20 +1,20 @@
 // Function to redirect based on user's choice
 function goToPage(page) {
     if (page === 'schedule') {
-        window.location.href = "schedule.html";
+        window.location.href = "schedule.html"; // Update with actual URL for schedule page
     } else if (page === 'feedback') {
-        window.location.href = "UserFeedbackHistory.html"; 
+        window.location.href = "UserFeedbackHistory.html"; // Update with actual URL for feedback page
     }
     else if (page === 'home2') {
-        window.location.href = "home2.html"; 
+        window.location.href = "home2.html"; // Update with actual URL for home2 page
     }
     else if (page === 'logout') {
         // window.location.href = "logout.html"; // Update to  sign out
         localStorage.removeItem('loggedInUser');
         localStorage.removeItem('redirectPage');
 
-        // Redirect to login page 
-        window.location.href = "../index.html";
+        // Redirect to login page (e.g., index.html)
+        window.location.href = "../Signin.html";  // Replace with the actual login page
 
     }
 }
